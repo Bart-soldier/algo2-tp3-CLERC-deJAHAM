c1f741f29f90e0b7e4dd692c54ae875fc05473fd
Bart-soldier <38186911+Bart-soldier@users.noreply.github.com>
2019-12-05 12:47:59 +0100

    Randomized version of Kruskal's algorithm implemented.

---
+++ main/src/spanningTree/RandomKruskalAlgorithm.java
@@ -0,0 +1,85 @@
+package src.spanningTree;
+
+import src.graphTools.Edge;
+import src.graphTools.Graph;
+
+import java.util.ArrayList;
+
+/**
+ * 3.3 Insertion aléatoire d’arêtes
+ */
+
+public class RandomKruskalAlgorithm {
+
+    public static ArrayList<Edge> SpanningTree(Graph graph) {
+        // List of edges
+        ArrayList<Edge> spanningTree = new ArrayList<>();
+
+        // List of all edges in the graph
+        ArrayList<Edge> graphEdges = new ArrayList<>();
+
+        // For each vertex in the graph
+        for(int vertex = 0 ; vertex < graph.order; vertex++) {
+            // For each edge of that vertex
+            for(Edge edge : graph.adjacency.get(vertex)) {
+                // If the edge isn't already in the list
+                if(!graphEdges.contains(edge)) {
+                    // Add it to the list
+                    graphEdges.add(edge);
+                }
+            }
+        }
+
+        // List of all edges in the graph in a random order
+        ArrayList<Edge> randomEdges = new ArrayList<>();
+
+        // Initialize randomEdges
+        while(graphEdges.size() > 0) {
+            // Take a random number between 0 and the size of graphEdges - 1
+            int randomNumber = (int) (Math.random() * graphEdges.size());
+
+            randomEdges.add(graphEdges.get(randomNumber));
+            graphEdges.remove(randomNumber);
+        }
+
+        // In order to keep track of the predecessor of the edge.
+        // If, when adding to the table, the edge does not have a predecessor, its predecessor is itself
+        int[] predecessor = new int[graph.order];
+        // Initialize predecessor
+        for(int i = 0; i < graph.order; i++) {
+            predecessor[i] = i;
+        }
+
+        // While we haven't look at all of the graph's edges
+        while(randomEdges.size() > 0) {
+            Edge edge = randomEdges.remove(0);
+
+            // Get the farthest predecessor from the source
+            int sourcePredecessor = parent(predecessor, edge.source);
+            // Get the farthest predecessor from the dest
+            int destPredecessor = parent(predecessor, edge.dest);
+
+            // If the sourcePredecessor is the same as the destPredecessor, i.e. they converge towards the same vertex
+            if(sourcePredecessor == destPredecessor){
+                // Then we have a cycle so we do nothing
+            }
+            else {
+                // Otherwise we don't have a cycle so we add the edge to the spanning tree
+                spanningTree.add(edge);
+                // We update the predecessor table
+                int newSourcePredecessor = parent(predecessor, sourcePredecessor);
+                int newDestPredecessor = parent(predecessor, destPredecessor);
+                predecessor[newDestPredecessor] = newSourcePredecessor;
+            }
+        }
+
+        return spanningTree;
+    }
+
+    private static int parent(int[] predecessor, int vertex){
+        // Look for predecessor until you find one that has none and return it
+        if(predecessor[vertex]!=vertex)
+            return parent(predecessor, predecessor[vertex]);;
+        return vertex;
+    }
+}
